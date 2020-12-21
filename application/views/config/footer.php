ce815b84a3635b9179715c336de425f5b83a29cc
ria2510 <riana.oktavia93@gmail.com>
2020-12-21 05:01:22 -0800

    project uas

---
+++ application/views/config/footer.php
@@ -0,0 +1,13 @@
+<div class="site-footer" style=" bottom: -220px">
+      
+      
+        
+         
+     
+        
+        <div class="copyright clearfix">
+          <p><b>UBSI Mart</b>&nbsp;&nbsp;&nbsp;&nbsp;<a href="getting-started.html">Hubungi kami ubsi </a>&nbsp;&bull;&nbsp;<p></p> Copyright UBSI 2020 
+        
+    </div>
+  </body>
+</html>
